9bda21db1a536dcf80ea90302e3548dd76222e92
barkode <konstantinks@gmail.com>
2024-08-11 22:59:19 +0100

    Added Google Maps with Markers

---
+++ assets/js/maps.js
@@ -0,0 +1,29 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: {
+      lat: 46.619261,
+      lng: -33.134766,
+    },
+  });
+
+  const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const locations = [
+    { lat: 40.789051, lng: -73.968285 },
+    { lat: 41.084045, lng: -73.874245 },
+    { lat: 40.754932, lng: -73.984016 },
+  ];
+
+  const markers = locations.map(
+    (location, i) =>
+      new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+      })
+  );
+
+  const markerCluster = new markerClusterer(map, markers, {
+    imagePath:
+      'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+  });
+}
